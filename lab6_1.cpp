9107db694ebde36d6cf921b4dd55bcbe5b0e3221
Axonosz <nattapat.is.me@gmail.com>
2025-12-12 15:51:09 +0700

    lab6.1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,22 +2,25 @@
 using namespace std;
 
 int main() {
-    int n;
-    int even_count = 0;
-    int odd_count = 0;
-
-    while (true) {
+    int number;
+    int evenCount = 0, oddCount = 0;
+    while(true){
         cout << "Enter an integer: ";
-        if (!(cin >> n)) return 0; 
-        if (n == 0) break; 
-        if (n % 2 == 0) ++even_count;
-        else ++odd_count;
+        cin >> number;
+        
+        if(number <= 0){
+            break;
+        }
+        
+        if(number % 2 ==0){
+            evenCount++;
+        } else{
+            oddCount++;
+        }
     }
-
-    cout << endl;
-    cout << "#Even numbers = " << even_count << endl;
-    cout << endl;
-    cout << "#Odd numbers = " << odd_count << endl;
-
+    
+    cout << "#Even numbers = "<< evenCount << endl;
+    cout << "#Odd numbers = " << oddCount << endl;
+    
     return 0;
 }
