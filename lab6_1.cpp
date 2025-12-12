b8689d321149acfce33720feb901509d6d009257
Axonosz <nattapat.is.me@gmail.com>
2025-12-12 15:30:46 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    int n;
+    int even_count = 0;
+    int odd_count = 0;
+
+    while (true) {
+        cout << "Enter an integer: ";
+        if (!(cin >> n)) return 0; 
+        if (n == 0) break; 
+        if (n % 2 == 0) ++even_count;
+        else ++odd_count;
+    }
+
+    cout << endl;
+    cout << "#Even numbers = " << even_count << endl;
+    cout << endl;
+    cout << "#Odd numbers = " << odd_count << endl;
+
     return 0;
 }
